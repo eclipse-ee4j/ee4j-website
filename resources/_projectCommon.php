6c59e7aa3131b79151f2673e7c2b7505b8fd97db
Wayne Beaton <wayne.beaton@eclipse-foundation.org>
2018-06-24 18:31:29 -0400

    Reconfigure the repository to exclusively provide the website source.
    This repository no longer plays "double duty". It now only contains the
    source that's needed to generate the website content. The generated
    content needs to be pushed separately (ideally via CI) to the website to
    actually make anything appear live at www.eclipse.org/ee4j.

--- resources/_projectCommon.php
+++ resources/_projectCommon.php
@@ -1,12 +1,14 @@
 <?php
 /*******************************************************************************
  * Copyright (c) 2017 Eclipse Foundation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://eclipse.org/legal/epl-v10.html
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
-$theme = "solstice";
+$theme = "quicksilver";
 $Nav->addNavSeparator("EE4J", "/ee4j");
 $Nav->addCustomNav("PMC Minutes", "/ee4j/minutes", "_self", NULL);
 $Nav->addCustomNav("PMC News", "/ee4j/news", "_self", NULL);
